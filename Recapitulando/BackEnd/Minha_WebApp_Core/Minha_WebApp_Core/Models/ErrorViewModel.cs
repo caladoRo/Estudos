35813fc636b87f5995eff9d03f51c663900b3572
caladoRo <rodrigo.jgc@hotmail.com>
2022-05-28 15:13:39 -0300

    Estruturação inicial recap

---
+++ Recapitulando/BackEnd/Minha_WebApp_Core/Minha_WebApp_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Minha_WebApp_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
