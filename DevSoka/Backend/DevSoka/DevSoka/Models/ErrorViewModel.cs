67044c0028764700634a56c07b065d31fee8f080
caladoRo <rodrigo.jgc@hotmail.com>
2022-05-28 14:25:14 -0300

    Estruturação inicial DevSoka

---
+++ DevSoka/Backend/DevSoka/DevSoka/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevSoka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
